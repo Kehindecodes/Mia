4bce37098ba806d31cdaf770119e76ee54936c0a
Oki Kehinde <olamiposi.oki@gmail.com>
2022-06-07 16:27:47 +0100

    finish salary screen desktop version

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './styles/App.scss';
 import ContactUs from './pages/ContactUs';
 import SelectGender from './components/SelectGender';
 import AfterSelection from './components/AfterSelection';
+import Salary from './components/Salary';
 
 function App() {
 	return (
 		<div>
 			{/* <ContactUs /> */}
 			{/* <SelectGender /> */}
-			<AfterSelection />
+			{/* <AfterSelection /> */}
+			<Salary />
 		</div>
 	);
 }
---
+++ src/components/Salary.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+import Layout from './Layout';
+import '../styles/Salary.scss';
+
+const Salary = () => {
+	return (
+		<Layout>
+			<Container>
+				<div className='form_wrapper'>
+					<Form>
+						<Form.Label className='mb-4'>
+							The Approximate Salary For A Maid Who Works 1 Hour Daily In Birr.
+							3000 To 4000 <br /> Per Month
+						</Form.Label>
+						<Form.Select>
+							<option>Select maximum salary</option>
+							<option value='1'>One</option>
+							<option value='2'>Two</option>
+							<option value='3'>Three</option>
+						</Form.Select>
+						<div className='btnwrapper mx-auto mt-3'>
+							<Button className='form_btn mt-4 ' type='submit'>
+								Continue
+							</Button>
+						</div>
+					</Form>
+				</div>
+			</Container>
+		</Layout>
+	);
+};
+
+export default Salary;
